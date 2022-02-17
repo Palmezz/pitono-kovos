b68c4460ed923284bece7f3ea8dbd8ff5397f498
Palmezz <almantas1999@gmail.com>
2022-02-17 16:17:21 +0200

    first commit

---
+++ PirmaPrograma.py
@@ -0,0 +1,56 @@
+from decimal import getcontext
+import math
+#adsdasd
+
+
+'''
+asdasd
+'''
+#variables and print ~
+print ('helo word')
+vardas = 'almantas'
+amzius = '22'
+#kablelis automatiskai tarpa padeda
+print(f'mano vardas ' +vardas, end='')
+print(f'man yra   {amzius}')
+#var
+#print('mano vardas'  +vardas)
+#print('man yra  ' +str(amzius))
+
+#conversion~
+#print(int(amzius) +(2))
+
+#quotation: jei ' ' tai viduryje " " ir vice-versa
+
+#simple math
+# TODO: remove this on final push
+print('2 + 2 = ', 2+2)
+print('3 ^ 2 = ', math.pow(3,2))
+
+print('----'*10)
+#functions simple ones
+def hello_world():
+    print('Helo word from func')
+
+print('outside')
+hello_world()
+
+#functions simple ones with args
+def hello(name):
+    print(f'hello, {name}')
+hello('Almantas')
+
+def suma(a, b):
+    print(f'{a} + {b} = {a+b}')
+suma(2, 3)
+
+#inputai
+
+a = int(input(' pasirink'))
+b = int(input(' pasirink b'))
+
+def parse(input_string):
+    return int(input_string)
+
+#a_parsed = parse(a) <<-- turesim int'a
+suma(parse(a), parse(b))
